867774fceaad3a36a830606fe0da09480918e210
Rodrigo Monteiro <rmonteir@icloud.com>
2024-12-01 20:33:09 -0300

    First chapter Docker application.

--- chapter-1-introduction-to-iac-and-aws-cdk/lib/chapter-1-introduction-to-iac-and-aws-cdk-stack.ts
+++ chapter-1-introduction-to-iac-and-aws-cdk/lib/chapter-1-introduction-to-iac-and-aws-cdk-stack.ts
@@ -1,16 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { ContainerImage } from 'aws-cdk-lib/aws-ecs';
+import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class Chapter1IntroductionToIacAndAwsCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Chapter1IntroductionToIacAndAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ApplicationLoadBalancedFargateService(this, 'MyWebServer', {
+      taskImageOptions: {
+        image: ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+      publicLoadBalancer: true
+    });
   }
 }
